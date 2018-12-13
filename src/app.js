b4be568dd576d30dbbde6c50125fb37159d2a8ca
Jørgen <mail@jvik.no>
2018-12-13 09:13:56 +0100

    Scraper is now working for weekdays

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 const rp = require("request-promise");
 const $ = require("cheerio");
 const url = "https://www.sundbaten.no/";
+var departures = { weekdayDepartures: [] };
 
 rp(url)
 	.then(function(html){
-		const outputInfo = $("td[data-label]", html)
-		const weekdayDepartures = [];
-		const result = Object.keys(outputInfo).map(function(key) {
-			return [Number(key), outputInfo[key]]
-		});
-		// console.log($("#hverdager > tbody", html).length);
-		result.forEach(element => {
-			
-		});
-		// console.log(weekdayDepartures.toString());
-		console.log(result)
+		const weekdayInput = $("td[data-label]", html)
+		for (let i = 0; i < weekdayInput.length; i++) {
+			let departureSite = weekdayInput[i].attribs["data-label"]
+			let departureTime = weekdayInput[i].children[0].data
+			departures.weekdayDepartures.push( {
+				"departureSite" : departureSite, 
+				"departureTime" : departureTime
+			}
+			)};
+		console.log(departures)
 	})
 	.catch(function(err){
 		throw new Error(err)
 	});
+
+app.get('/', (req, res) => res.json(departures))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
