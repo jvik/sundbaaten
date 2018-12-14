884b80298bbdf96a103e6a42a82f6fb7978aeec9
Jørgen <mail@jvik.no>
2018-12-14 21:56:27 +0100

    Scraping for all weekdays work, but must be refactored a lot

--- src/app.js
+++ src/app.js
@@ -2,28 +2,56 @@ const express = require('express')
 const app = express()
 const port = 4005
 
+// const pretty = require("express-prettify");
 const rp = require("request-promise");
 const $ = require("cheerio");
 const url = "https://www.sundbaten.no/";
-var departures = { weekdayDepartures: [] };
+var departures = { 
+	weekdayDepartures: [],
+	saturdayDepartures: [],
+	sundayDepartures: []
+};
 
-rp(url)
+const test = rp(url)
 	.then(function(html){
-		const weekdayInput = $("td[data-label]", html)
-		for (let i = 0; i < weekdayInput.length; i++) {
-			let departureSite = weekdayInput[i].attribs["data-label"]
-			let departureTime = weekdayInput[i].children[0].data
-			departures.weekdayDepartures.push( {
-				"departureSite" : departureSite, 
+		const weekdaySelector = $("#hverdager > tbody > tr > td", html)
+		for (let i = 0; i < weekdaySelector.length; i++) {
+			let departureSite = weekdaySelector[i].attribs["data-label"]
+			let departureTime = weekdaySelector[i].children[0].data
+			departures.weekdayDepartures.push({
+				"departureSite" : departureSite,
 				"departureTime" : departureTime
-			}
-			)};
-		console.log(departures)
-	})
-	.catch(function(err){
-		throw new Error(err)
+			});
+		}
+
+		const saturdaySelector = $("#lordager > tbody > tr > td", html)
+		for (let i = 0; i < saturdaySelector.length; i++) {
+			let departureSite = saturdaySelector[i].attribs["data-label"]
+			let departureTime = saturdaySelector[i].children[0].data
+			departures.saturdayDepartures.push({
+				"departureSite" : departureSite,
+				"departureTime" : departureTime
+			});
+		}
+
+		const sundaySelector = $("#sondager > tbody > tr > td", html)
+		for (let i = 0; i < sundaySelector.length; i++) {
+			let departureSite = sundaySelector[i].attribs["data-label"]
+			let departureTime = sundaySelector[i].children[0].data
+			departures.sundayDepartures.push({
+				"departureSite" : departureSite,
+				"departureTime" : departureTime
+			});
+		}
+		
+		const jsonStringedDepartures = JSON.parse(JSON.stringify(departures))
+		return jsonStringedDepartures
 	});
 
-app.get('/', (req, res) => res.json(departures))
+app.set("json spaces", 2)
+
+app.get('/', async (req, res) => 
+	await res.json(test)
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
